96bff1a60e75b28b96ccc2f626cd0a7e21bb5f73
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2023-10-24 04:48:00 +0900

    updated print_hex

--- ft_print_hex.c
+++ ft_print_hex.c
@@ -6,10 +6,37 @@
 /*   By: hmitsuyo <yourLogin@student.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 03:15:43 by hmitsuyo          #+#    #+#             */
-/*   Updated: 2023/10/24 03:16:15 by hmitsuyo         ###   ########.fr       */
+/*   Updated: 2023/10/24 04:47:45 by hmitsuyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	print_hex(unsigned int number)
+#include "ft_printf.h"
+
+static char	*hex_to_str(unsigned int number, int flag)
 {
+	char	*result;
+	int		digits;
+
+	digits = unsigned_digits(number);
+	result = (char *)malloc(sizeof(char) * (digits + 1));
+	if (result == NULL)
+		return (NULL);
+
+
+	return (result);
+}
+
+int	print_hex(unsigned int number, int flag)
+{
+	int		length;
+	char	*s;
+
+	length = 0;
+	s = hex_to_str(number, flag);
+	if (s == NULL)
+		return (0);
+	length = ft_strlen(s);
+	write(1, s, length);
+	free(s);
+	return (length);
 }
